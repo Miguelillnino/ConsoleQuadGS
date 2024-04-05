a11c6dfa53cd3438fa7e31b9cd5fb7a5151728ec
Miguel de Jesús Martínez Felipe <mmartinezf2002@alumno.ipn.mx>
2024-04-05 11:16:14 -0600

    05/04/2024  11:15 Miguel Martinez
    Quadrilateral detection 1.0.0

--- ConsoleQuad/ConsoleQuad.cpp
+++ ConsoleQuad/ConsoleQuad.cpp
@@ -1,160 +1,197 @@
 
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <vector>
-#include <numeric>
 #include <cmath>
-#include <chrono>
-
-    /**
-    * Convert an Int Vector to Float Vector.
-    *
-    * @param values Container a vector `intVector`.
-    * @return the vector of `floatVector`.
-    */
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include <numeric>
+#include <vector>
 
 /*
-std::vector<double> castToIntVectorToDouble(const std::vector<int>& intVector) {
-    std::vector<double> DoubleVector(intVector.size());
-
-    // Iterate over each element and cast to float
-    for (size_t i = 0; i < intVector.size(); ++i) {
-        DoubleVector[i] = static_cast<float>(intVector[i]);
-    }
-
-    return DoubleVector;
-}
+*******************************************************
+******************************************************
+*  Version of quadrilateral-detection software 1.0.0" 
+*******************************************************
+*******************************************************
+* Sequencial program that detects quadrilateral polygons
+* Fuctions:
+*   angleCos()
+*   findQuads()
+*   main()
+********************************************************
+******************************************************
+*******************************************************
+******************************************************
 */
 
 
 
 /**
-    * substract two Vectors.
-    *
-    * @param values Container two vectors `v1` adn `v2`.
-    * @return the double vector of `result`.
-    */
-
-std::vector<double> subtractDoubleVectors(const std::vector<double>& v1, const std::vector<double>& v2) {
-    // Both vectors have the same dimension
-    if (v1.size() != v2.size()) {
-        std::cerr << "Error: Vectors must have the same size for subtraction." << std::endl;
-        return std::vector<double>(); // Return an empty vector
-    }
-
-    // Create a vector to store the result
-    std::vector<double> result;
-    result.reserve(v1.size()); // Reserve memory for efficiency
-
-    // Subtract corresponding elements
-    for (size_t i = 0; i < v1.size(); ++i) {
-        result.push_back(v1[i] - v2[i]);
-    }
-
-    return result;
-}
-
-/**
-    * Calculates the cosine of the angle between two vectors
+    * Calculates the cosine of the angle between Points from Vector
     * The cosine of the angle between two vectors can be calculated
     * using the dot product and the magnitudes of the vectors.
     *
-    * @param values Container three vectors `p0`, `p1` and , `p2`.
-    * @return the double vector of `result`.
+    * @param values Container three 2d-Points `p0`, `p1` and , `p2`.
+    * @return the double of `result`.
     */
 
 
-double angle_cos(const std::vector<double>& p0, const std::vector<double>& p1, const std::vector<double>& p2){
-    double angleCosResult = -3.14;
-    std::vector<double> d1 = subtractDoubleVectors(p0, p1);
-    std::vector<double> d2 = subtractDoubleVectors(p2, p1);
-    
-    //COROBORAR QUE EL PRODUCTO INTERNO SE DEVUELVA COMO vector o double
-    double aData = std::inner_product(d1.begin(), d1.end(), d2.begin(), 0.0);
-    double bData = std::inner_product(d1.begin(), d1.end(), d1.begin(), 0.0);
-    double cData = std::inner_product(d2.begin(), d2.end(), d2.begin(), 0.0);
-
-    angleCosResult = fabs(aData / sqrt(bData*cData) );
-
-    return angleCosResult;
+double angleCos(cv::Point pt1, cv::Point pt2, cv::Point pt0) {
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1 * dx2 + dy1 * dy2) / std::sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
 }
 
 
+/**
+    * Returns a sequence of quadrilaterals detected on the image.
+    * Metholody:
+    *   -Canny
+    *   -Dilation
+    *   -To Find Countours
+    *   -Aprroximation of Poligonal Curves
+    *   -Poligon 4 Vertices, Convex and Area greater than 1000
+    *   -Calculate maximum cosine between angles
+    *   -All angles are ~90 degree
+    *
+    * @param values Container a cv::Mat image(Gray-Scale) and Vector<Vector<Point>> quads
+    * @return the double of `result`.
+    */
+
+static void findQuads(const cv::Mat& image, std::vector<std::vector<cv::Point> >& quads)
+{
+    quads.clear();
+    
+    cv::Mat edges;
+    cv::Mat dilate;
+
+    for (double thrs = 0; thrs < 255; thrs += 26)
+    {
+        //Detect Edges in Gray Image
+        cv::Canny(image, edges, thrs, 255.0, 5);
+        //Defined the kernel of dilation
+        cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2));
+        //Dilate the Gray image
+        int iterations = 1; // Number of iterations
+        cv::dilate(edges, dilate, kernel, cv::Point(-1, -1), iterations);
+
+        std::vector<std::vector<cv::Point>> contornos;
+
+        if (!dilate.empty()) {
+            cv::findContours(dilate, contornos, cv::RETR_LIST, cv::CHAIN_APPROX_NONE);
+        }
+        std::vector<cv::Point> approx;
+
+        // Test each contour
+        for (size_t i = 0; i < contornos.size(); i++)
+        {
+            // approximate contour with accuracy proportional
+            // to the contour perimeter
+
+            cv::approxPolyDP(contornos[i], approx, cv::arcLength(contornos[i], true) * 0.02, true);
+
+            // quads contours should have 4 vertices
+            // relatively large area (to filter out noisy contours)
+            // and be convex.
+            // area may be positive or negative - in accordance with the
+            // contour orientation
+            if (approx.size() == 4 &&
+                fabs(cv::contourArea(approx)) > 1000 && cv::isContourConvex(approx))
+            {
+                double maxCosine = 0;
+                for (int j = 2; j < 5; j++)
+                {
+                    // find the maximum cosine of the angle between joint edges
+                    double cosine = fabs(angleCos(approx[j % 4], approx[j - 2], approx[j - 1]));
+                    maxCosine = MAX(maxCosine, cosine);
+                }
+
+                // if cosines of all angles are small
+                // (all angles are ~90 degree) then save a quad
+                if (maxCosine < 0.3) {
+                    quads.push_back(approx);
+                }
+            }
+        }
+    }
+
+ }
 
 
-
+/*
+*Testing quads detction using the camera by default.
+* 
+*/
 
 int main()
 {
     std::cout << "Version of quadrilateral-detection software 1.0.0" << std::endl;
 
-    // Get the current time before starting the operation
-    auto start = std::chrono::high_resolution_clock::now();
-    // Get the current time after finishing the operation
-    auto end = std::chrono::high_resolution_clock::now();
-    
-    //Open a Video Cam from one device
-    cv::VideoCapture cap_video;
-    cap_video.open(0);
+    int deviceID = 0; // 0 = open default camera
+    int apiID = cv::CAP_ANY; // 0 = autodetect default API
 
-    if (cap_video.isOpened() == false) {
+    //Initialize Camera and reading the frames from the camera
+    cv::VideoCapture cap_video;
+    cap_video.open(deviceID, apiID);
+
+    if (!cap_video.isOpened()) {
         std::cout << "Error reading video file" << std::endl;
+        return -1;
     }
 
+    //Set up the camera 
+    
     //std::cout << cap.get(cv::CV_CAP_PROP_FPS) << std::endl;
     std::cout << cap_video.get(cv::CAP_PROP_FRAME_COUNT) << std::endl;
     cap_video.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('U', 'Y', 'V', 'Y'));
-    
     cap_video.set(cv::CAP_PROP_FRAME_WIDTH, 640);
     cap_video.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-
     double Width = cap_video.get(cv::CAP_PROP_FRAME_WIDTH);
     double Height = cap_video.get(cv::CAP_PROP_FRAME_HEIGHT);
-
     std::cout << "Width= "<< Width << std::endl;
     std::cout << "Height= " << Height << std::endl;
 
-    auto cap_duration = std::chrono::seconds(10);
-    auto current = std::chrono::high_resolution_clock::now();
+    std::cout << "Start grabbing" << std::endl
+        << "Press any ESC to terminate" << std::endl;
+    
+   /*
+    * Code to read Frame by Frame
+    */
 
-    while((current - start) < cap_duration){
-
-        cv::Mat frame;
-        cap_video >> frame;
-
+    cv::Mat frame; 
+    for (;;)
+    {
+        cap_video.read(frame);
         if (frame.empty())
             break;
-
-        cv::imshow("Frame", frame);
-
-        char c = (char) cv::waitKey(25);
-
-        if (c == 27)
+        
+        //Convert image in gray scale
+        cv::Mat imgGray;
+        cv::cvtColor(frame,imgGray, cv::COLOR_BGR2GRAY);
+        std::vector<std::vector<cv::Point>> quads;
+        
+        findQuads(imgGray, quads);
+   
+        //Print detected quads
+        for (const auto& quad : quads) {
+            std::cout << "Quad:" << std::endl;
+            for (const auto& point : quad) {
+                std::cout << "(" << point.x << ", " << point.y << ")" << std::endl;
+            }
+            std::cout << std::endl;
+        }
+        //draw quads in image from frame
+        cv::polylines(imgGray, quads, true, cv::Scalar(255, 0, 0), 3, cv::LINE_AA);
+        cv::imshow("Imagen", imgGray);
+        
+        // Press ESC for exit
+        char c = (char)cv::waitKey(25);
+        if (cv::waitKey(30) == 27) { 
             break;
         }
         
-
-
-    //std::cout << cv::getBuildInformation() << std::endl;
-
-    
-    
-    
-    
-    // Calculate the duration of the operation
-    std::chrono::duration<double> duration = end - start;
-    // Print the duration in seconds
-    std::cout << "Time taken: " << duration.count() << " seconds" << std::endl;
-
-
-    
+    }
 }
-
-//This code is to calculate the max_cos
-/*
-double max_cos = -1; // Initialize with a value less than -1
-for (int i = 0; i < 4; ++i) {
-    double cos_value = angle_cos(cnt[i], cnt[(i + 1) % 4], cnt[(i + 2) % 4]);
-    max_cos = std::max(max_cos, cos_value);
-}
-*/
